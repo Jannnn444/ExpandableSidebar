ee6ce7f738aa8f7b218fbfdd56c1f3946ebae6ac
Jannnn444 <yucianhuang@gmail.com>
2024-10-04 14:38:14 +0800

    Have the sidebar shows with black opacity backgrounda

--- Sidebar/Sidebar.swift
+++ Sidebar/Sidebar.swift
@@ -9,14 +9,19 @@ import SwiftUI
 
 struct Sidebar: View {
     @Binding var isSidebarVisible: Bool
-
+    
     var body: some View {
-        if isSidebarVisible {
-            Text("Sidebar visible")
-                .bold()
-                .font(.largeTitle)
-                .background(.purple)
+        ZStack {
+            GeometryReader { _ in
+                EmptyView()
+            }
+            .background(.black.opacity(0.6))
+            .opacity(isSidebarVisible ? 1 : 0)
+            .animation(.easeInOut.delay(0.2), value: isSidebarVisible)
+            .onTapGesture {
+                isSidebarVisible.toggle()
+            }
         }
+        .edgesIgnoringSafeArea(.all)
     }
 }
-

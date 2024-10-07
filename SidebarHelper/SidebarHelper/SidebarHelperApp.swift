4f49ea4c75ae55e87f98bd6d5f536df74630337e
Jannnn444 <yucianhuang@gmail.com>
2024-10-07 22:04:32 +0800

    Add the reference project

---
+++ SidebarHelper/SidebarHelper/SidebarHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SidebarHelperApp.swift
+//  SidebarHelper
+//
+//  Created by yucian huang on 2024/10/5.
+//
+
+import SwiftUI
+
+@main
+struct SidebarHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
